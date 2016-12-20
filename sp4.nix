d2ab430263923d3c7e0cd4c8eaba8a8795b3d1d1
George Stoianov <gstoianov@gmail.com>
2016-12-20 12:29:29 +0200

    Nix binary cache public key for sp4

--- sp4.nix
+++ sp4.nix
@@ -51,6 +51,22 @@
     };
   };
 
+  # nix.binaryCaches = [
+  #   "https://cache.nixos.org/"
+
+  #   # This assumes that you use the default `nix-serve` port of 5000
+  #   "http://192.168.0.100:5001"
+  # ];
+
+  nix.binaryCachePublicKeys = [
+    "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+
+    # Replace the following string with the contents of the
+    # `nix-serve.pub` file you generated in the "Server configuration"
+    # section above
+    "192.168.0.100:0Hvx/nyjrKng7On9LCqwj4bSxYmiDSscTazXXdIJLFY="
+  ];
+
   networking = {
     hostName = "nixos-sp4";
     extraHosts =
